1ede9ce39259eb636f8b162d8772602bd15f6660
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-03-19 14:18:36 +0530

    add : product context

---
+++ src/context/DataContextProvider.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, createContext, useReducer } from "react";
+import { FilterReducer, productReducer } from "../reducer";
+
+const StateContext = createContext();
+
+const DataContextProvider = ({ children }) => {
+  const [data, dispatch] = useReducer(productReducer, {
+    isDataLoading: false,
+    product: [],
+    cart: [],
+    wishList: [],
+    categories: [],
+  });
+  const [filterState, filterDispatch] = useReducer(FilterReducer, {
+    inStock: false,
+    fastDelivery: false,
+    byRating: 0,
+    sort: "",
+    priceRange: "",
+    filterByCategories: [],
+  });
+
+  return (
+    <StateContext.Provider
+      value={{ data, dispatch, filterState, filterDispatch }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
+
+export default DataContextProvider;
