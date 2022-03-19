de90699662460069e0e4bf2803a8ebdbaa92a934
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-03-19 14:15:19 +0530

    add : warp context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import DataContextProvider from "./context/DataContextProvider";
+
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
